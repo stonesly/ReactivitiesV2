d9ae17f87932b8ff6cae48600b5c32b36786396b
Scott Willock <swillock@doctorlogic.com>
2021-09-03 00:43:49 -0500

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import './app/layout/styles.css';
+import { store, StoreContext } from './app/stores/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
